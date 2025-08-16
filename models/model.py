321813512aea8138b2e7b758c409cb9d24570a2f
Violet Yu Chieh Hsu <ujfiderciah20120512@gmail.com>
2025-08-16 21:20:18 +1000

    Add files via upload

--- models/model.py
+++ models/model.py
@@ -4,26 +4,27 @@ from keras_tuner import Objective
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import regularizers as reg
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.calibration import CalibratedClassifierCV
 
 # Build keras model
-def build_model(input_dim:int, units1=64, units2=32, units3=16, lr=0.001):
+def build_model(input_dim:int, units1=64, units2=32, units3=16, lr=1e-3, l2=1e-4, drop=0.2):
     # Initialise ANN
     model = Sequential()
     
     # Input layer and 1st hidden layer
-    model.add(Dense(units1, kernel_initializer='he_uniform', activation='relu', input_shape=(input_dim,)))
+    model.add(Dense(units1, kernel_initializer='he_uniform', activation='relu', input_shape=(input_dim,), kernel_regularizer=reg.l2(l2)))
     model.add(BatchNormalization())
     model.add(Dropout(0.2))
     
     # 2nd layer
-    model.add(Dense(units2, kernel_initializer='he_uniform', activation='relu'))
+    model.add(Dense(units2, kernel_initializer='he_uniform', activation='relu', kernel_regularizer=reg.l2(l2)))
     model.add(BatchNormalization())
     model.add(Dropout(0.2))
     
     # 3rd layer
-    model.add(Dense(units3, kernel_initializer='he_uniform', activation='relu'))
+    model.add(Dense(units3, kernel_initializer='he_uniform', activation='relu', kernel_regularizer=reg.l2(l2)))
     model.add(BatchNormalization())
     
     # Output layer
@@ -34,11 +35,13 @@ def build_model(input_dim:int, units1=64, units2=32, units3=16, lr=0.001):
     
     return model 
 
-def hypermodel(hp:kt.HyperParameters, input_dim:int):
+def hypermodel(hp, input_dim:int):
     u1 = hp.Int('units1', min_value=32, max_value=128, step=32)
-    u2 = hp.Int('units2', min_value=16, max_value=64, step=16)
-    u3 = hp.Int('units3', min_value=8, max_value=32, step=8)
+    u2 = hp.Int('units2', min_value=32, max_value=128, step=32)
+    u3 = hp.Int('units3', min_value=16, max_value=64, step=16)
     lr = hp.Float('lr', 1e-4, 1e-2, sampling='log')
+    l2 = hp.Float('l2', 1e-6, 1e-3, sampling='log')
+    dr = hp.Float('drop', 0.1, 0.5, step=0.1)
     return build_model(input_dim, u1, u2, u3, lr)
 
 def make_tuner(input_dim:int, project_name='krs_hyperband', directory='hyperband'):
